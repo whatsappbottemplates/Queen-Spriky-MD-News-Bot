992a6d7e8ecd749e9aa3ebb066ee0b68ad58372e
whatsappbottemplates <botudavin@gmail.com>
2024-09-24 15:41:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || ''    // Enter Your Bot Number
+    SESSION_ID: process.env.SESSION_ID || 'M20ShKLC#k49kx727mZsAZbVmOg9cmlg4BakWjIWGm-apWGXDkL8', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://spriky:1234@cluster0.sx9pb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
 };
