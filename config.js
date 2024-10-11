c28de64c4d632e5408db97b20d0a518cf604d3dc
whatsappbottemplates <botudavin@gmail.com>
2024-10-11 11:25:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'M20ShKLC#k49kx727mZsAZbVmOg9cmlg4BakWjIWGm-apWGXDkL8', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://elisaqueen525:<db_password>@cluster0.qf2vrdx.mongodb.net/',    // Enter Your MongoDB URL
+    MONGODB: process.env.MONGODB || 'mongodb+srv://elisaqueen525:1234@cluster0.qf2vrdx.mongodb.net/',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
     BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
 };
